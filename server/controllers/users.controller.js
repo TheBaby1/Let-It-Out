f0146813005ca144e09cb3e3de2d7065dd6351c1
Nikko Ensomo <nikkonuriel@gmail.com>
2025-07-18 01:23:55 +0800

    Added get all users route

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -9,10 +9,18 @@ export const createUser = async (req, res) => {
 
     try {
         const newUser = new User(user);
-
         await newUser.save();
         res.status(201).json(newUser);
     } catch (error) {
         res.status(500).json({ message: 'Error creating user', error: error.message });
+    }
+}
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching users', error: error.message });
     }
 }
