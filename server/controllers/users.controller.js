0a24b1f8b50cd55d04be287eaec6ce2155c6318b
Nikko Ensomo <nikkonuriel@gmail.com>
2025-07-18 01:39:26 +0800

    Added update user by id route

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -38,3 +38,18 @@ export const deleteUserById = async (req, res) => {
         res.status(500).json({ message: 'Error deleting user', error: error.message });
     }
 }
+
+export const updateUserById = async (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
+
+    try {
+        const user = await User.findByIdAndUpdate(id, updatedUser);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'User updated successfully' });
+    } catch (error) {
+        res.status(500).json({ message : 'Error updating user', error: error.message });
+    }
+}
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -3,6 +3,7 @@ import { healthCheck } from '../controllers/users.controller.js';
 import { createUser } from '../controllers/users.controller.js';
 import { getAllUsers } from '../controllers/users.controller.js';
 import { deleteUserById } from '../controllers/users.controller.js';
+import { updateUserById } from '../controllers/users.controller.js';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.get('/health', healthCheck);
 router.post('/users/create-user', createUser);
 router.get('/users/get-all-users', getAllUsers);
 router.delete('/users/delete-user/:id', deleteUserById);
+router.put('/users/update-user/:id', updateUserById);
 
 export default router;
